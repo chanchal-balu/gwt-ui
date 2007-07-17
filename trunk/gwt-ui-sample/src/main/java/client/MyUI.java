e7ea5e624f940370a99d9f0fbf3918a0eef0730c
olostan <olostan@b56c78aa-3334-0410-acf8-4d94f2448e8f>
2007-07-17 19:04:46 +0000

    Eclipse project for sample

--- trunk/gwt-ui-sample/src/main/java/client/MyUI.java
+++ trunk/gwt-ui-sample/src/main/java/client/MyUI.java
@@ -1,13 +1,15 @@
 package client;
 
 import com.google.gwt.user.client.ui.Panel;
+
+import org.olostan.gwtui.client.NotificationSender;
 import org.olostan.gwtui.client.UIManager;
 
 /**
  * Created by Olostan
  * Date: 30.06.2007 19:44:15
  */
-public interface MyUI extends UIManager {
+public interface MyUI extends UIManager, NotificationSender {
     public Panel getMainContainer();
     public Panel getCenterContentContainer();
 }
